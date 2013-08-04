5233c8ba3670537c06f313e87df1ac5c67bc32a8
Tanya L. Sweeney <tanyalsweeney@yahoo.com>
2013-08-04 13:46:03 -0700

    Add identity info and start on household info for data structure

---
+++ js/questionList.js
@@ -0,0 +1,247 @@
+{
+  "identity" : [
+    {
+      "label" : "id0010",
+      "text" : "First Name",
+      "dataType": "string",
+      "inputType" : "text",
+      "validate" : "^[A-Za-z]+$",
+      "cssClass" : ""
+      },{
+      "label" : "id0020",
+      "text" : "Middle Name",
+      "dataType": "string",
+      "inputType" : "text",
+      "validate" : "^[A-Za-z]+$",
+      "cssClass" : ""
+    },{
+      "label" : "id0030",
+      "text" : "Last Name",
+      "dataType": "string",
+      "inputType" : "text",
+      "validate" : "^[A-Za-z]+$",
+      "cssClass" : ""
+    },{
+      "label" : "id0040",
+      "text" : "Suffix",
+      "dataType": "string",
+      "inputType" : "select",
+      "cssClass" : "",
+      "answerOptions" : [
+        "Sr",
+        "jr",
+        "III",
+        "IV"
+      ]
+    },{
+      "label" : "id0050",
+      "text" : "Client ID Number (if known)",
+      "dataType": "string",
+      "inputType" : "text",
+      "cssClass" : "line-end"
+    },{
+      "label" : "id0060",
+      "text" : "Street address where you live",
+      "dataType": "string",
+      "inputType" : "text",
+      "cssClass" : ""
+    },{
+      "label" : "id0070",
+      "text" : "City",
+      "dataType": "string",
+      "inputType" : "text",
+      "validate" : "^[A-Za-z]+$",
+      "cssClass" : ""
+    },{
+      "label" : "id0080",
+      "text" : "State",
+      "dataType": "string",
+      "inputType" : "text",
+      "cssClass" : "",
+      "answerOptions" : [
+      	"WA",
+      	"Other"
+      ]
+    },{
+      "label" : "id0090",
+      "text" : "Zip Code",
+      "dataType": "string",
+      "inputType" : "text",
+      "cssClass" : ""
+    },{
+      "label" : "id0100",
+      "text" : "Preferred Phone Number",
+      "dataType": "string",
+      "inputType" : "text",
+      "cssClass" : "line-end"
+    },{
+      "label" : "id0110",
+      "text" : "Mailing Address (if different)",
+      "dataType": "string",
+      "inputType" : "text",
+      "cssClass" : ""
+    },{
+      "label" : "id0120",
+      "text" : "City",
+      "dataType": "string",
+      "inputType" : "text",
+      "validate" : "^[A-Za-z]+$",
+      "cssClass" : ""
+    },{
+      "label" : "id0130",
+      "text" : "State",
+      "dataType": "string",
+      "inputType" : "text",
+      "cssClass" : "",
+      "answerOptions" : [
+      	"WA",
+      	"Other"
+      ]
+    },{
+      "label" : "id0140",
+      "text" : "Zip Code",
+      "dataType": "string",
+      "inputType" : "text",
+      "cssClass" : ""
+    },{
+      "label" : "id0150",
+      "text" : "Other Phone Number",
+      "dataType": "string",
+      "inputType" : "text",
+      "cssClass" : ""
+    },{
+      "label" : "id0160",
+      "text" : "First Name",
+      "dataType": "string",
+      "inputType" : "text",
+      "validate" : "^[A-Za-z]+$",
+      "cssClass" : ""
+      },{
+      "label" : "id0020",
+      "text" : "Middle Name",
+      "dataType": "string",
+      "inputType" : "text",
+      "validate" : "^[A-Za-z]+$",
+      "cssClass" : ""
+    },{
+      "label" : "id0030",
+      "text" : "Last Name",
+      "dataType": "string",
+      "inputType" : "text",
+      "validate" : "^[A-Za-z]+$",
+      "cssClass" : ""
+    },{
+      "label" : "id0040",
+      "text" : "Suffix",
+      "dataType": "string",
+      "inputType" : "select",
+      "cssClass" : "",
+      "answerOptions" : [
+        "Sr",
+        "jr",
+        "III",
+        "IV"
+      ]
+    }
+
+  ],"householdMembers" : [
+  	{
+      "label" : "hm0010",
+      "text" : "First Name",
+      "dataType": "string",
+      "inputType" : "text",
+      "validate" : "^[A-Za-z]+$",
+      "cssClass" : ""
+      },{
+      "label" : "hm0020",
+      "text" : "Middle Name",
+      "dataType": "string",
+      "inputType" : "text",
+      "validate" : "^[A-Za-z]+$",
+      "cssClass" : ""
+    },{
+      "label" : "hm0030",
+      "text" : "Last Name",
+      "dataType": "string",
+      "inputType" : "text",
+      "validate" : "^[A-Za-z]+$",
+      "cssClass" : ""
+    },{
+      "label" : "hm0040",
+      "text" : "Suffix",
+      "dataType": "string",
+      "inputType" : "select",
+      "cssClass" : "",
+      "answerOptions" : [
+        "Sr",
+        "jr",
+        "III",
+        "IV"
+      ]
+    },{
+      "label" : "hm0050",
+      "text" : "Sex M or F",
+      "dataType": "string",
+      "inputType" : "text",
+      "validate" : "^[A-Za-z]+$",
+      "cssClass" : ""
+    },{
+      "label" : "hm0060",
+      "text" : "How is this person related to you?",
+      "dataType": "string",
+      "inputType" : "text",
+      "default" : "Myself",
+      "validate" : "^[A-Za-z]+$",
+      "cssClass" : ""
+    },{
+      "label" : "hm0070",
+      "text" : "Date of Birth",
+      "dataType": "string",
+      "inputType" : "text",
+      "validate" : "",
+      "cssClass" : ""
+    },{
+      "label" : "hm0080",
+      "text" : "Check if you want benefits for this person",
+      "dataType": "boolean",
+      "inputType" : "text",
+      "validate" : "^[A-Za-z]+$",
+      "cssClass" : ""
+    },{
+      "label" : "hm0090",
+      "text" : "Social Security Number",
+      "dataType": "string",
+      "inputType" : "text",
+      "validate" : "",
+      "cssClass" : ""
+    },{
+      "label" : "hm0100",
+      "text" : "Check if US Citezen",
+      "dataType": "boolean",
+      "inputType" : "checkbox",
+      "validate" : "",
+      "cssClass" : ""
+    },{
+      "label" : "hm0110",
+      "text" : "",
+      "dataType": "string",
+      "inputType" : "text",
+      "validate" : "^[A-Za-z]+$",
+      "cssClass" : ""
+    },{
+      "label" : "hm0120",
+      "text" : "Race",
+      "dataType": "string",
+      "inputType" : "text",
+      "validate" : "^[A-Za-z]+$",
+      "cssClass" : ""
+    },{
+      "label" : "hm0130",
+      "text" : "Tribe Name (\"for American Indians and Alaska Navites\")",
+      "dataType": "string",
+      "inputType" : "text",
+      "validate" : "^[A-Za-z]+$",
+      "cssClass" : "line-end"
+    }
+  ]
+}
